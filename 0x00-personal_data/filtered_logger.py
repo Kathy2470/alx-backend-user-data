05f713d9afc970e2fbb0f4a53a04a086b3f63612
kathy2470 <mukisakathy@gmail.com>
2024-07-31 21:18:54 +0300

    filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -7,6 +7,7 @@ This module contains a function to obfuscate specific fields in log messages.
 import re
 from typing import List
 
+
 def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
     """
     Obfuscates specified fields in a log message.
