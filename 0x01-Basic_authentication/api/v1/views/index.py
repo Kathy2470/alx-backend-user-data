96dddb41ed1de19d8235e0676e4b7181112d3c6d
kathy2470 <mukisakathy@gmail.com>
2024-08-06 18:18:44 +0300

    Task 2 index.py update

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -2,13 +2,15 @@
 """
 Module of Index views
 """
+
 from flask import jsonify, abort
 from api.v1.views import app_views
 
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
-    """ GET /api/v1/status
+    """
+    GET /api/v1/status
     Return:
       - the status of the API
     """
@@ -17,7 +19,8 @@ def status() -> str:
 
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
-    """ GET /api/v1/stats
+    """
+    GET /api/v1/stats
     Return:
       - the number of each objects
     """
@@ -27,9 +30,10 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
-def unauthorized() -> str:
-    """ GET /api/v1/unauthorized
-    Raise a 401 error
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
     """
-    abort(401)
+    GET /api/v1/forbidden
+    Raise a 403 error
+    """
+    abort(403)
