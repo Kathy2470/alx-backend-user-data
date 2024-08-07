f646eb040704a6d09dc1ff978f427ab5ad1bddbf
kathy2470 <mukisakathy@gmail.com>
2024-08-07 23:50:05 +0300

    update

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -24,6 +24,7 @@ elif auth_type == "session_auth":
     from api.v1.auth.session_auth import SessionAuth
     auth = SessionAuth()
 
+
 @app.before_request
 def before_request():
     """ Method to handle requests before they are processed """
@@ -41,7 +42,26 @@ def before_request():
             if auth.current_user(request) is None:
                 abort(403)
 
+
+@app.errorhandler(404)
+def not_found(error) -> str:
+    """ Not found handler """
+    return jsonify({"error": "Not found"}), 404
+
+
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """ Unauthorized handler """
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """ Forbidden handler """
+    return jsonify({"error": "Forbidden"}), 403
+
+
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    app.run(host=host, port=int(port))
+    app.run(host=host, port=port)
--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,6 +1,8 @@
-# api/v1/auth/session_auth.py
-
+#!/usr/bin/env python3
+""" SessionAuth module
+"""
 from api.v1.auth.auth import Auth
 
 class SessionAuth(Auth):
+    """ SessionAuth class that inherits from Auth """
     pass
