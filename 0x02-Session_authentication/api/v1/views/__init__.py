afed28570540936b934a1c3e48120448be4dc1dc
kathy2470 <mukisakathy@gmail.com>
2024-08-10 00:49:49 +0300

    update

--- 0x02-Session_authentication/api/v1/views/__init__.py
+++ 0x02-Session_authentication/api/v1/views/__init__.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
 """ API v1 views initialization
 """
+
 from flask import Blueprint
 
-# Importing the blueprint for session_auth
+# Importing blueprints for session_auth
 from api.v1.views.session_auth import auth as session_auth
 
 # Initialize Blueprint
