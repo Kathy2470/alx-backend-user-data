b07a74e14d33ef40cf8ca444a4713b3d49f9d097
kathy2470 <mukisakathy@gmail.com>
2024-08-10 00:11:46 +0300

    Task 6 Session_auth.py

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,27 +1,41 @@
 #!/usr/bin/env python3
-""" SessionAuth module for handling session authentication
+"""SessionAuth class that handles session authentication
 """
-import uuid
+
 from api.v1.auth.auth import Auth
+from models.user import User
+import uuid
 
 
 class SessionAuth(Auth):
-    """ SessionAuth class that manages session authentication """
-
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
-        """ Create a session ID for a given user_id """
-        if user_id is None or not isinstance(user_id, str):
+        """Creates a session ID for a user_id"""
+        if user_id is None or type(user_id) is not str:
             return None
-
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
-        """ Returns a User ID based on a Session ID """
-        if session_id is None or not isinstance(session_id, str):
+        """Returns a User ID based on a session ID"""
+        if session_id is None or type(session_id) is not str:
             return None
-
         return self.user_id_by_session_id.get(session_id)
+
+    def session_cookie(self, request=None):
+        """Returns the session cookie value from a request"""
+        if request is None:
+            return None
+        return request.cookies.get(self.session_name)
+
+    def current_user(self, request=None):
+        """Returns a User instance based on a cookie value"""
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return None
+        user_id = self.user_id_for_session_id(session_id)
+        if user_id is None:
+            return None
+        return User.get(user_id)
