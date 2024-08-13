93104d4d98459f655934f1723bfe2bba28ca0df6
root <kathy2470>
2024-08-13 04:14:01 -0700

    update

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -1,17 +1,25 @@
 #!/usr/bin/env python3
-""" Users view module
 """
+Users view module
+"""
+
 from api.v1.app import app
 from models.user import User
 from api.v1.auth import auth
 
+
 def get_users():
-    """Get all users"""
+    """
+    Get all users
+    """
     users = User.all()
     return jsonify([user.to_dict() for user in users])
 
+
 def get_user(user_id):
-    """Get a user"""
+    """
+    Get a user
+    """
     if user_id == 'me':
         if request.current_user is None:
             abort(404)
@@ -21,21 +29,30 @@ def get_user(user_id):
         abort(404)
     return jsonify(user.to_dict())
 
+
 def create_user():
-    """Create a user"""
+    """
+    Create a user
+    """
     user = User.create(request.get_json())
     return jsonify(user.to_dict()), 201
 
+
 def update_user(user_id):
-    """Update a user"""
+    """
+    Update a user
+    """
     user = User.get(user_id)
     if user is None:
         abort(404)
     user.update(request.get_json())
     return jsonify(user.to_dict())
 
+
 def delete_user(user_id):
-    """Delete a user"""
+    """
+    Delete a user
+    """
     user = User.get(user_id)
     if user is None:
         abort(404)
