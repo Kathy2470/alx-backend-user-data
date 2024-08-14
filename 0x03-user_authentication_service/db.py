7b7ecdf2f4d3d653616b8c7b82e961a924c609ea
root <kathy2470>
2024-08-14 01:40:36 -0700

    update

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -2,45 +2,46 @@
 """
 DB module
 """
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.exc import InvalidRequestError
-from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.orm.session import Session
+
+from user import Base, User
+
 
 class DB:
+    """DB class
     """
-    DB class
-    """
-    def __init__(self):
-        """
-        Initialize DB instance
-        """
-        self._session = self.create_session()
 
-    def create_session(self):
+    def __init__(self) -> None:
+        """Initialize a new DB instance
         """
-        Create a new database session
-        """
-        engine = create_engine('mysql://root:root@localhost/my_db')
-        Session = sessionmaker(bind=engine)
-        return Session()
+        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
 
-    def find_user_by(self, **kwargs):
+    @property
+    def _session(self) -> Session:
+        """Memoized session object
         """
-        Find a user by arbitrary keyword arguments
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
+
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """Add a user to the database.
 
         Args:
-            **kwargs: arbitrary keyword arguments
+            email (str): The user's email address.
+            hashed_password (str): The user's hashed password.
 
         Returns:
-            User: the first user found matching the keyword arguments
-
-        Raises:
-            InvalidRequestError: if the keyword arguments are invalid
-            NoResultFound: if no user is found matching the keyword arguments
+            User: The newly created User object.
         """
-        try:
-            return self._session.query(User).filter_by(**kwargs).first()
-        except InvalidRequestError:
-            raise
-        except Exception as e:
-            raise e
+        new_user = User(email=email, hashed_password=hashed_password)
+        self._session.add(new_user)
+        self._session.commit()
+        return new_user
