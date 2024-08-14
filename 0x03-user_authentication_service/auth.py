3f500a595a01b7af74c7ab5a96f46b3cc3635c73
root <kathy2470>
2024-08-14 02:13:04 -0700

    update

---
+++ 0x03-user_authentication_service/auth.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Auth module
+"""
+import bcrypt
+
+def _hash_password(password: str) -> bytes:
+    """Hashes a password with a salt using bcrypt.
+
+    Args:
+        password (str): The password to hash.
+
+    Returns:
+        bytes: The salted hash of the password.
+    """
+    # Generate a salt
+    salt = bcrypt.gensalt()
+
+    # Hash the password using the generated salt
+    hashed_password = bcrypt.hashpw(password.encode(), salt)
+
+    return hashed_password
