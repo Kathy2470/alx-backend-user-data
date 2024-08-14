40780a385cbbf02fe3c5d37a443a9c3d1d08406d
root <kathy2470>
2024-08-14 02:40:28 -0700

    app.py update

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,15 +2,19 @@
 """
 Flask app module
 """
+
 from flask import Flask, jsonify
 
+
 app = Flask(__name__)
+
 
 @app.route("/", methods=["GET"])
 def home():
     """Returns a JSON payload with a welcome message.
     """
     return jsonify(message="Bienvenue")
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
