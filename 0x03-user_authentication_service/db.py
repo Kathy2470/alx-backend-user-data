c60a1f688a29a2ed060a21b567b43fc2dcc21bff
root <kathy2470>
2024-08-14 01:38:26 -0700

    update

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -2,73 +2,45 @@
 """
 DB module
 """
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.session import Session
-from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.exc import InvalidRequestError
-
-from user import Base, User
-
+from sqlalchemy.orm.exc import NoResultFound
 
 class DB:
-    """DB class
     """
-
-    def __init__(self) -> None:
-        """Initialize a new DB instance
+    DB class
+    """
+    def __init__(self):
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
-        Base.metadata.drop_all(self._engine)
-        Base.metadata.create_all(self._engine)
-        self.__session = None
-
-    @property
-    def _session(self) -> Session:
-        """Memoized session object
+        Initialize DB instance
         """
-        if self.__session is None:
-            DBSession = sessionmaker(bind=self._engine)
-            self.__session = DBSession()
-        return self.__session
+        self._session = self.create_session()
 
-    def add_user(self, email: str, hashed_password: str) -> User:
-        """Add a user to the database.
+    def create_session(self):
+        """
+        Create a new database session
+        """
+        engine = create_engine('mysql://root:root@localhost/my_db')
+        Session = sessionmaker(bind=engine)
+        return Session()
+
+    def find_user_by(self, **kwargs):
+        """
+        Find a user by arbitrary keyword arguments
 
         Args:
-            email (str): The user's email address.
-            hashed_password (str): The user's hashed password.
+            **kwargs: arbitrary keyword arguments
 
         Returns:
-            User: The newly created User object.
-        """
-        new_user = User(email=email, hashed_password=hashed_password)
-        self._session.add(new_user)
-        self._session.commit()
-        return new_user
-
-    def find_user_by(self, **kwargs) -> User:
-        """Find a user by arbitrary keyword arguments.
-
-        Args:
-            **kwargs: Arbitrary keyword arguments to filter the users.
-
-        Returns:
-            User: The first user that matches the filtering criteria.
+            User: the first user found matching the keyword arguments
 
         Raises:
-            NoResultFound: If no user is found.
-            InvalidRequestError: If any of the query arguments are invalid.
+            InvalidRequestError: if the keyword arguments are invalid
+            NoResultFound: if no user is found matching the keyword arguments
         """
-        if not kwargs:
-            raise InvalidRequestError("No filter criteria provided.")
-
-        query = self._session.query(User)
-
         try:
-            return query.filter_by(**kwargs).one()
-        except NoResultFound:
-            raise NoResultFound("No user found with the given criteria.")
+            return self._session.query(User).filter_by(**kwargs).first()
+        except InvalidRequestError:
+            raise
         except Exception as e:
-            raise InvalidRequestError(f"An error occurred: {e}")
+            raise e
